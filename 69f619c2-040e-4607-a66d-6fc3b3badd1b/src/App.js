285fea8209c3faa4e225c74743811b0b28668a0a
Wil Wilsman <hello@wilwilsman.com>
2019-03-30 17:36:11 -0500

    Add a router

--- 69f619c2-040e-4607-a66d-6fc3b3badd1b/src/App.js
+++ 69f619c2-040e-4607-a66d-6fc3b3badd1b/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Router, Redirect } from '@reach/router';
+
 import Root from './components/Root';
 import Team from './pages/Team';
 
 export default function App() {
   return (
-    <Root>
-      <Team/>
-    </Root>
+    <Router>
+      <Root path="/">
+        <Team path="/teams"/>
+        {/* redirect since there are no other top-level routes */}
+        <Redirect from="*" to="/teams" noThrow/>
+      </Root>
+    </Router>
   );
 }
