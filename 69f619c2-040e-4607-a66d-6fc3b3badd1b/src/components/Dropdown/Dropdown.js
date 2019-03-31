2d89112ddd7dd6251df4b8354ff294416df995ec
Wil Wilsman <hello@wilwilsman.com>
2019-03-31 16:22:39 -0500

    Refine breadcrumb dropdown

--- 69f619c2-040e-4607-a66d-6fc3b3badd1b/src/components/Dropdown/Dropdown.js
+++ 69f619c2-040e-4607-a66d-6fc3b3badd1b/src/components/Dropdown/Dropdown.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, createRef } from 'react';
 import { Link } from '@reach/router';
 import classnames from 'classnames/bind';
 
@@ -7,9 +7,21 @@ const cx = classnames.bind(style);
 
 export default function Dropdown({ title, items, ...props }) {
   let [isOpen, toggleOpen] = useState(false);
+  let ref = createRef();
+
+  useEffect(() => {
+    let onClick = e => {
+      if (ref.current && !ref.current.contains(e.target)) {
+        toggleOpen(false);
+      }
+    };
+
+    document.addEventListener('click', onClick);
+    return () => document.removeEventListener('click', onClick);
+  });
 
   return (
-    <span {...props} className={cx('Dropdown', { isOpen })}>
+    <span {...props} className={cx('Dropdown', { isOpen })} ref={ref}>
       <span onClick={() => toggleOpen(!isOpen)}>
         {title}
       </span>
