1c6ff37e571fb415229056407d8ded8166cd3551
Wil Wilsman <hello@wilwilsman.com>
2019-03-30 20:26:11 -0500

    Style members list

--- 69f619c2-040e-4607-a66d-6fc3b3badd1b/src/pages/Members.js
+++ 69f619c2-040e-4607-a66d-6fc3b3badd1b/src/pages/Members.js
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from '@reach/router';
+import classnames from 'classnames/bind';
 import axios from 'axios';
+
 import * as style from '../App.less';
+
+const cx = classnames.bind(style);
 
 function useMembers(teamId) {
   let [members, setMembers] = useState([]);
@@ -21,13 +25,12 @@ export default function Members({ location, navigate, teamId }) {
   // map data to link objects for the breadcrumb component
   members = members.map(member => {
     let linkTo = `/teams/${teamId}/members/${member.id}`;
-    return { ...member, linkTo };
+    let isActive = location.pathname.indexOf(linkTo) === 0;
+    return { ...member, linkTo, isActive };
   });
 
   // get the active link to check if we need to redirect
-  let activeLink = members.find(({ linkTo }) => {
-    return location.pathname.indexOf(linkTo) === 0;
-  });
+  let activeLink = members.find(({ isActive }) => isActive);
 
   if (!activeLink && members.length) {
     // navigatge to the first member's details page
@@ -35,14 +38,25 @@ export default function Members({ location, navigate, teamId }) {
   }
 
   return (
-    <div className={style.Box}>
+    <div className={style.MemberList}>
       <ul>
         {members.map((member, i) => (
           <li key={i}>
-            <Link to={member.linkTo}>
-              <span>
+            <Link
+              to={member.linkTo}
+              className={cx({
+                isActive: member.isActive
+              })}
+            >
+              <span className={style.MemberListName}>
                 {member.name.first}
+                &nbsp;
                 {member.name.last}
+              </span>
+              <span className={style.MemberListLocation}>
+                {member.location.city}
+                ,&nbsp;
+                {member.location.state}
               </span>
             </Link>
           </li>
